0e80f16c2dddcd98f492ebed8a5bdfe02e1e0e0e
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-26 19:52:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,8 +27,6 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  ".headers on;"
-  ".mode column"
-  "width auto"
-  "SELECT * FROM bears;"
+  
+  ".schema"
 end
